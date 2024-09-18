abd8ee681997c64fa7bde8761edad235ad848329
Anurag Jyoti <anuragjyoti.dev@gmail.com>
2024-09-19 00:03:45 +0530

    added back button

--- src/components/back.js
+++ src/components/back.js
@@ -5,7 +5,7 @@ const BackButton = () => {
         <div className='w-full h-[40px] bg-slate-700 flex items-center px-4 sm:px-[30px] text-white'>
         <Link href='/'>
             <button className='flex flex-row gap-2 sm:gap-[3px] font-semibold'>
-                <span>🡨</span><span>Back</span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg></span><span>Back</span>
             </button>
         </Link>
     </div>
